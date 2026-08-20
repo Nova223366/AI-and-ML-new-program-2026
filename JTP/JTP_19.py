68fe2b6ee21f2c00dfc148cd7329969cd47f1508
Nova223366 <jaiaditya49@gmail.com>
2026-08-20 21:22:12 +0530

    HW -old

---
+++ JTP/JTP_19.py
@@ -0,0 +1,10 @@
+def show (n, string="Good", k=5): #(n, string, "Good", k=5)
+    return display(string)
+    return n
+
+def display (string):
+    return string == str(5)
+
+print(show('Children/s day'))
+print(display(string = 'true'))
+print(show(5, 'Good'))
